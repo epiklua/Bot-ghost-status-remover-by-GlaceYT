c4d3ff82909eaaeed74d751ef39a6cea822f6f72
epicscriptor <subbewastaken@gmail.com>
2024-08-04 17:50:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Most Trusted MM Server!","ROBLOX KINGDOM GOATED"];
 
 
 let currentIndex = 0;
