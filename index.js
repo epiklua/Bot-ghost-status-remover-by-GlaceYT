3689a6e02f79760cba668511b5e90250c047a65a
epicscriptor <subbewastaken@gmail.com>
2024-08-14 11:09:05 -0700

    index.jsupdate
    e

--- index.js
+++ index.js
@@ -36,8 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Verified by EpicWare as Trusted Middleman Bot","Most Trusted Middleman Bot!", "Secured Data Saving and Trade History!", "Created with EpicWare!", "Most Certified Middleman Bot"];
-
+const statusMessages = ["Created with EpicWare", "Fully Functional", "Official EpicWare Bot"]
 
 let currentIndex = 0;
 const channelId = '';
